6db5f102a952436d38e7dd2f09314866625fee2b
FemenSB <felipe_sborsato@hotmail.com>
2024-06-04 12:20:10 -0300

    fixup! Implement drop behavior

--- src/pages/voting/page.component.tsx
+++ src/pages/voting/page.component.tsx
@@ -2,12 +2,12 @@ import CandidateList from './candidate_list.component';
 
 export default function VotingPage() {
   const candidates = [
-    '0',
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
+    'Brandt',
+    'Adrianna',
+    'Avis',
+    'Jones',
+    'Rochelle',
+    'Carmella',
   ];
 
   return (
