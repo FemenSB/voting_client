7be1d83669e3525672804c43e55482d1b528c6d0
FemenSB <felipe_sborsato@hotmail.com>
2024-06-06 09:42:41 -0300

    fixup! Implement navigation
    This fixes the security issue caused by <a target="_blank"> without rel="noreferrer"

--- src/routes/about/page.component.tsx
+++ src/routes/about/page.component.tsx
@@ -7,7 +7,7 @@ export default function AboutPage() {
     <Fragment>
       Ranked Voting Online is an open-source project developed by Felipe Borsato
       <br />
-      Elements from its UI are based on <a href='https://www.figma.com/community/file/958198956095698455/tini-design-system' target='_blank'>Tini Design System</a>, licensed under <a href='https://creativecommons.org/licenses/by/4.0/' target='_blank'>CC BY 4.0</a>
+      Elements from its UI are based on <a href='https://www.figma.com/community/file/958198956095698455/tini-design-system' target='_blank' rel='noreferrer'>Tini Design System</a>, licensed under <a href='https://creativecommons.org/licenses/by/4.0/' target='_blank' rel='noreferrer'>CC BY 4.0</a>
     </Fragment>
   );
 }
