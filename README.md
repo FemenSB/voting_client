edc38823eff56f161c3b7e1031ab1f74094ebc88
FemenSB <felipe_sborsato@hotmail.com>
2024-06-08 15:00:46 -0300

    Add Inter typeface
    Tini Design System uses the Inter typeface.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Ranked Voting Online is a platform where users can conduct quick ranked-choice v
 # Licenses
 
 Elements from this project's UI are based on the [Tini Design System](https://www.figma.com/community/file/958198956095698455/tini-design-system), licensed under [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/)
+
+This project uses the Inter typeface, licensed under the [SIL Open Font License](/open_font_license.txt).
--- src/routes/about/page.component.tsx
+++ src/routes/about/page.component.tsx
@@ -5,9 +5,11 @@ import { Fragment } from 'react';
 export default function AboutPage() {
   return (
     <Fragment>
-      Ranked Voting Online is an open-source project developed by Felipe Borsato
+      Ranked Voting Online is an <a href='https://github.com/FemenSB/voting_client'target='_blank' rel='noreferrer'>open-source</a> project developed by Felipe Borsato
       <br />
       Elements from its UI are based on <a href='https://www.figma.com/community/file/958198956095698455/tini-design-system' target='_blank' rel='noreferrer'>Tini Design System</a>, licensed under <a href='https://creativecommons.org/licenses/by/4.0/' target='_blank' rel='noreferrer'>CC BY 4.0</a>
+      <br />
+      This project uses the Inter typeface, licensed under the <a href='/open_font_license.txt' target='_blank' rel='noreferrer'>SIL Open Font License</a>.
     </Fragment>
   );
 }
